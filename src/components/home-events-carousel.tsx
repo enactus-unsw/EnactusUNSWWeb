5d5712bade962bb3d301c51fcf8ddaba3390601d
Amy <z5481175@ad.unsw.edu.au>
2025-03-23 20:25:40 +1100

    fixed events on homepage

--- src/components/home-events-carousel.tsx
+++ src/components/home-events-carousel.tsx
@@ -3,26 +3,27 @@ import { Card, CardMedia, Box } from "@mui/material";
 import { ArrowBackRounded, ArrowForwardRounded } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
-const oWeekImage = "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/476229220_122121672578607827_495259889061321110_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=75d36f&_nc_ohc=3wFz0zQxaZMQ7kNvgEurH_0&_nc_oc=AdihBjLQO6Vv6XxsJmYp_98dneI7R-LJb7S6mqZtkDj7PCR3p4DyVDHS5Mh5hG4_gyo&_nc_zt=23&_nc_ht=scontent.fsyd3-1.fna&_nc_gid=AAVm2pcF-cwY8ElaYAK2UBo&oh=00_AYDy_cAAkEh7wGI0NIyfm9kvExh4YvmGxE12xM58yPzivw&oe=67B8DBCF";
-const directorRecruitmentImage = "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/475761069_590907987195402_7232662635612807952_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=75d36f&_nc_ohc=VN-zWsrTz7kQ7kNvgGBfsgC&_nc_oc=Adil9KLPLS-7F6RpR7EXnAzshhcWg0RDpt5JbW2eCONOFSOd6GS_r_SwWVnwgyw_bQA&_nc_zt=23&_nc_ht=scontent.fsyd3-1.fna&_nc_gid=ArvPpUuRf-q4I61SaTFe0If&oh=00_AYCQpQJ8PYjrqrX7tNJD7PZcUCIx72yjHHERBdfELkSU2w&oe=67B8EA66";
-const launchWeekImage = "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/480322642_122122994588607827_6406124203658938237_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=75d36f&_nc_ohc=HuFUJazQIgYQ7kNvgEqnvWW&_nc_oc=AdiyzpTeAwGfQSOi8d5Fqdxk0wQyCwXytxKqJjyFxybQYfT4TKDrtu777V51o-wAAdE&_nc_zt=23&_nc_ht=scontent.fsyd3-1.fna&_nc_gid=AI-UwbyWO7y5yArtiqTves0&oh=00_AYDdDj9DvSAFbaNp_YE2u0YALzCOgjHGpaQb0lvAoT15BA&oe=67B910A0";
+// Images of the event cover pictures
+import launchWeekImage from "../images/eventsCoverPic/launchWeek.jpg";
+import pitchForImpact from "../images/eventsCoverPic/pitchForImpact.jpg";
+import projectWeekAndShowcase from "../images/eventsCoverPic/projectWeekAndShowcase.jpg";
 
 const events = [
+  {
+    title: "Pitch for Impact Case Challenge",
+    link: "https://www.facebook.com/share/18GECyQyGp/",
+    image: pitchForImpact,
+  },
+  {
+    title: "Project Week & Showcase 2025",
+    link: "https://fb.me/e/aZwJn0xIK",
+    image: projectWeekAndShowcase,
+  },
   {
     title: "Launch Week",
     link: "https://www.facebook.com/events/1091196176028342",
     image: launchWeekImage,
-  },
-  {
-    title: "O-Week Event",
-    link: "https://www.facebook.com/events/480811088409092",
-    image: oWeekImage,
-  },
-  {
-    title: "2025 Director Recruitment",
-    link: "https://www.facebook.com/events/400736763044830/",
-    image: directorRecruitmentImage,
-  },
+  }
 ];
 
 export default function HomeEventsCarousel() {
