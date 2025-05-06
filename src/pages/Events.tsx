6696790105c76e3f18258cbbe41181749c323ca6
Jess O <z5479430@ad.unsw.edu.au>
2025-05-06 19:18:47 +1000

    header for events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,8 +1,14 @@
 
 export default function Events() {
   return (
-    <div>
-        events
-    </div>
+    <>
+      <header className="text-center">
+        <div className="ml-auto mr-auto m-4 mt-10 mt-10 mb-10 rounded-xl bg-gradient-to-r from-[#FFF1CA] to-[#FFC222] w-11/12">
+          <h1 className="font-bold text-4xl p-6">
+            Events
+          </h1>
+        </div>
+      </header>
+    </>
   )
 }
