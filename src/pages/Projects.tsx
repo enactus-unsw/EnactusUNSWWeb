1aea59bdd888f46dd528efe87b9f3fbacf483baa
pranaviparasu <144297441+pranaviparasu@users.noreply.github.com>
2025-05-26 18:11:08 +1000

    reduced font size, removed learn more buttons (#10)

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -15,14 +15,14 @@ export default function Projects() {
         </div>
         {/* Prockets description */}
         <div className="flex flex-col w-1/2 p-2 pr-20 ml-[4.5rem]">
-          <div className="font-[700] mt-20 text-6xl">
+          <div className="font-[700] mt-20 text-4xl">
             Prockets
           </div>
-          <p className="items-center justify-center text-2xl leading-10 font-[400] mt-12">
+          <p className="items-center justify-center text-xl leading-10 font-[400] mt-12">
             Creating prosthetics more accessible to amputees - financially by developing a cost-effective component of the prosthetic and socially through a support network platform.
           </p>
           <div className="flex flex-row items-center mt-10">
-            <button className="bg-[#FFC222] hover:bg-yellow-700 text-2xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
+            <button className="bg-[#FFC222] hover:bg-yellow-700 text-xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
               LEARN MORE
             </button>
             <a href="https://www.instagram.com/prockets.au/" target="_blank" rel="noopener noreferrer">
@@ -37,19 +37,19 @@ export default function Projects() {
       <div className="flex flex-row justify-center bg-[#F6F6F6] rounded-t-[5vw] p-5 pl-20 pr-20 min-h-[25vh]">
         {/* SharePlate description */}
         <div className="flex flex-col w-1/2 p-2 ml-[12rem]">
-          <div className="font-[700] mt-20 text-6xl tracking-tight">
+          <div className="font-[700] mt-20 text-4xl tracking-tight">
             Shareplate
           </div>
-          <p className="items-center justify-center text-2xl leading-10 font-[400] mt-12">
+          <p className="items-center justify-center text-xl leading-10 font-[400] mt-12">
             A not for profit initiative dedicated to bridging the gap between households experiencing food insecurity and those who have the capacity to provide resources through a digital platform known as 'Shareplate'.           
           </p>
 
           <div className="flex flex-row items-center mt-10">
-            <button className="bg-[#FFC222] hover:bg-yellow-700 text-2xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
+            {/* <button className="bg-[#FFC222] hover:bg-yellow-700 text-2xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
               LEARN MORE
-            </button>
+            </button> */}
             <a href="https://www.instagram.com/shareplate.au/" target="_blank" rel="noopener noreferrer">
-              <img className="h-15 block ml-4" src="src/images/Logos/Instagram.png" alt="Instagram" />
+              <img className="h-15 block" src="src/images/Logos/Instagram.png" alt="Instagram" />
             </a>
           </div>
         </div>
@@ -68,17 +68,17 @@ export default function Projects() {
           </div>
           {/* Tech4Impact description */}
           <div className="flex flex-col w-1/2 p-2">
-            <div className="font-[700] mt-20 text-6xl tracking-tight">
+            <div className="font-[700] mt-20 text-4xl tracking-tight">
               Tech4Impact
             </div>
-            <p className="items-center justify-center text-2xl leading-10 font-[400] mt-12 mr-[6rem]">
+            <p className="items-center justify-center text-xl leading-10 font-[400] mt-12 mr-[6rem]">
               Tech4Impact is a project focused on helping those with limited access to technology overcome the digital divide. We refurbish old devices, giving them new life and donating them to those in need. Our goal is to increase technology access; creating equal opportunities for education, employment, and connectivity.
             </p>
 
             <div className="flex flex-row items-center mt-[3rem]">
-              <button className="bg-[#FFC222] hover:bg-yellow-700 text-2xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
+              {/* <button className="bg-[#FFC222] hover:bg-yellow-700 text-2xl font-bold py-2 px-4 rounded-2xl h-[3.5rem] w-[15rem]">
                 LEARN MORE
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
