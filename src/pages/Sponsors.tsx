c5d6c4c9fda52a7ac2173c8b38e45007d12f513d
pranaviparasu <144297441+pranaviparasu@users.noreply.github.com>
2025-06-26 13:09:48 +1000

    made sponsers page mobile responsive (#22)

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -14,34 +14,37 @@ export default function Sponsors() {
   return (
     <div>
       <header style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <img src={Banner} alt="Banner" className="w-full h-auto" style={{ marginTop: '59px' }} />
+        <img src={Banner} alt="Banner" className="w-full h-auto" style={{ marginTop: '3rem' }} />
         <h1
-          style={{
-            position: 'absolute',
-            color: '#2A2D32',
-            fontWeight: 'bold',
-            fontSize: 'clamp(24px, 5vw, 60px)',
-            margin: 0,
-            marginTop: 0,
-            zIndex: 1,
-            textAlign: 'center',
-            width: '100%',
-          }}
+            className="
+            absolute 
+            text-[#2A2D32] 
+            font-bold 
+            text-lg
+            sm:text-xl
+            md:text-9xl
+            md:mb-60 
+            z-[1] 
+            text-center 
+            w-full
+          "
         >
           Sponsors and Partners
         </h1>
         <h3
-          style={{
-            position: 'absolute',
-            color: '#2A2D32',
-            fontWeight: 400,
-            fontSize: 'clamp(16px, 2vw, 34px)',
-            margin: 0,
-            marginTop: 'clamp(100px, 15vw, 190px)',
-            zIndex: 2,
-            textAlign: 'center',
-            width: '100%',
-          }}
+         className="
+            absolute 
+            text-[#2A2D32] 
+            font-normal 
+            text-xs
+            sm:text-lg
+            md:text-6xl
+            mt-20
+            md:mt-60
+            z-[2] 
+            text-center 
+            w-full
+          "
         >
           Enactus UNSW is grateful for all our wonderful <br></br> sponsors and partners!
         </h3>
@@ -54,7 +57,7 @@ export default function Sponsors() {
             fontWeight: 'bold',
             fontSize: 'clamp(30px, 5vw, 45px)',
             margin: 0,
-            marginTop: '100px',
+            marginTop: "10vh",
             zIndex: 3,
             textAlign: 'center',
             width: '100%',
@@ -63,7 +66,7 @@ export default function Sponsors() {
         Sponsors
       </h2>
 
-      <img src={Separator} alt="Separator" className="w-full h-auto" style={{ width: '1200px', margin: '0 auto', display: 'block', marginTop: '28px'}}/>
+      <img src={Separator} alt="Separator" className="w-full h-auto" style={{ width: '90vw', margin: '0 auto', display: 'block', marginTop: '28px'}}/>
       
       <img src={commonwealthbank} alt="Commonwealth Bank" className="w-auto h-auto" style={{width: '600px', margin: '0 auto', display: 'block', marginTop: '49px'}}/>
       
@@ -83,17 +86,15 @@ export default function Sponsors() {
         Partners
       </h2>
 
-      <img src={Separator} alt="Separator" className="w-full h-auto" style={{ width: '1200px', margin: '0 auto', display: 'block', marginTop: '28px', marginBottom: '95px'}}/>
+      <img src={Separator} alt="Separator" className="w-full h-auto" style={{ width: '90vw', margin: '0 auto', marginTop: 30, display: 'block', marginBottom: 50}}/>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-        <img src={thescarycanary} alt="The Scary Canary" className="w-auto h-auto" style={{ width: '225px', marginLeft: '100px', marginRight: '77px' }}/>
-        <img src={capgemini} alt="Capgemini" className="w-auto h-auto" style={{ width: '496px', marginRight: '12px' }}/>
-        <img src={macquarie} alt="Macquarie" className="w-auto h-auto" style={{ width: '423px' }}/>
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '106px' }}>
-        <img src={redbull} alt="Red Bull" className="w-auto h-auto" style={{ width: '372px' }}/>
-        <img src={bearish} alt="Bearish" className="w-auto h-auto" style={{ width: '405px' }}/>
+      
+      <div className="w-fit mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-x-2 gap-y-2 mt-2 place-items-center">
+        <img src={thescarycanary} alt="The Scary Canary" className="w-[140px] sm:w-[180px] md:w-[225px]"/>
+        <img src={capgemini} alt="Capgemini" className="w-[240px] sm:w-[350px] md:w-[496px]"/>
+        <img src={macquarie} alt="Macquarie" className="w-[200px] sm:w-[320px] md:w-[423px]"/>
+        <img src={redbull} alt="Red Bull" className="w-[200px] sm:w-[320px] md:w-[372px]"/>
+        <img src={bearish} alt="Bearish" className="w-[200px] sm:w-[320px] md:w-[405px]"/>
       </div>
     </div>
   );
