b42b91620287c4af75d8bf98f6a2525ffcac9380
Amy <z5481175@ad.unsw.edu.au>
2025-07-04 21:24:47 +1000

    added page animations when going to a new page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
-import {  Routes, Route, HashRouter } from "react-router";
+import { HashRouter, Routes, Route, useLocation } from "react-router";
+import { AnimatePresence } from "framer-motion";
 
 import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
@@ -10,9 +11,10 @@ import ContactUs from "../pages/ContactUs";
 
 import Header from "../components/header"; 
 import Footer from "../components/footer"; 
+import ScrollToTop from "../components/scroll-to-top";
+import PageWrapper from "../components/page-wrapper";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import ScrollToTop from "../components/scroll-to-top";
 
 const theme = createTheme({
   typography: {
@@ -20,26 +22,35 @@ const theme = createTheme({
   },
 });
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/about-us" element={<PageWrapper><AboutUs /></PageWrapper>} />
+        <Route path="/our-team" element={<PageWrapper><OurTeam /></PageWrapper>} />
+        <Route path="/projects" element={<PageWrapper><Projects /></PageWrapper>} />
+        <Route path="/events" element={<PageWrapper><Events /></PageWrapper>} />
+        <Route path="/sponsors-partners" element={<PageWrapper><Sponsors /></PageWrapper>} />
+        <Route path="/contact-us" element={<PageWrapper><ContactUs /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <HashRouter>
-        <ScrollToTop /> 
-        <Header/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/our-team" element={<OurTeam />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/sponsors-partners" element={<Sponsors />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-          </Routes>
-          <Footer/>
+        <ScrollToTop />
+        <Header />
+        <AnimatedRoutes />
+        <Footer />
       </HashRouter>
     </ThemeProvider>
   );
 }
 
 export default App;
-
