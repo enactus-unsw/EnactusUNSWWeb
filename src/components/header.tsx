0b0c57956bde30bef04c78b1cd8508c316f91544
PiyushJ1 <piyxsh@gmail.com>
2026-03-09 15:30:45 +1100

    refactor: move links towards the centre of the page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,17 @@ const enactusLogo = '/images/Enactus Logo.png';
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-md">
-      <nav className="mx-auto flex max-w-full flex-nowrap items-center justify-around px-6 sm:justify-between sm:overflow-x-auto md:px-10 lg:px-20">
-        <div className="align-self: flex-start flex min-w-2 shrink-0">
+      <nav className="mx-auto flex max-w-full flex-nowrap items-center px-6 sm:overflow-x-auto md:px-10 lg:px-20">
+        <div className="flex shrink-0">
           <Link to="/">
             <img
-              className="m-3 h-10 h-16 w-auto"
+              className="m-3 h-16 w-auto"
               src={enactusLogo}
               alt="Enactus UNSW Logo"
             />
           </Link>
         </div>
-        <div className="align-self: flex-end flex space-x-16 max-sm:hidden">
+        <div className="flex flex-1 items-center justify-center space-x-16 max-sm:hidden">
           <Link
             to="/about-us"
             className="font-Montserrat py-2 text-xl font-bold text-[#2A2D32] hover:underline"
@@ -54,15 +54,17 @@ const Header = () => {
           >
             CONTACT
           </Link>
+        </div>
+        <div className="ml-auto flex shrink-0 items-center">
           <Link
             to={{ pathname: '/', hash: '#join-us' }}
             rel="noopener noreferrer"
-            className="inline-block rounded-xl bg-[#FFC222] px-5 py-3 text-center font-bold text-black hover:bg-[#FFDA55]"
+            className="inline-block rounded-xl bg-[#FFC222] px-5 py-3 text-center font-bold text-black hover:bg-[#FFDA55] max-sm:hidden"
           >
             JOIN US
           </Link>
+          <Dropdown />
         </div>
-        <Dropdown />
       </nav>
     </header>
   );
